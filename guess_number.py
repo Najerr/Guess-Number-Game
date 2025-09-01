10065ad0d9a81a720671850ecb9c26f970939ddc
Dmitriy Shimchuk <shimchuk2501@mail.ru>
2025-09-01 12:47:32 +0700

    Format code

--- guess_number.py
+++ guess_number.py
@@ -2,13 +2,17 @@ from random import randint
 
 
 def get_random_number():
-    number = randint(0,101)
-    return number 
+    number = randint(0, 101)
+    return number
+
+
 a = get_random_number()
+
+
 # print(a)
 def guess_number():
     player_number = int(input())
-    
+
     if player_number < a:
         print("Ваше число меньше того, что загадано")
         guess_number()
@@ -20,4 +24,3 @@ def guess_number():
 
 
 guess_number()
-
